cfa24b0d083d7c97f9c947ac878fc2019b3e815d
anhhhhthuuu <86774791+cassie1610@users.noreply.github.com>
2021-07-02 11:00:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     You are the only one that
     take my mind somewhere else `,
     btnIntro: 'hihi',
-    title: 'Do you wanna be my Greentea in the morning 😙',
-    desc: 'be my, my sugar honey ',
+    title: 'Do you wanna be my Greentea in the morning, be my sugar, honey? 😙',
+    desc: 'if you dont click the answer, it means you love me ',
     btnYes: 'yea, you are all I need <33',
     btnNo: 'you are just so great, but I cant be with u :3',
     question:'there are over 7 billion in the word Why were you trying to get some time with me <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'I know it 🥰. big butt 😘😘',
     messDesc: 'why dont you msg me immediately.',
     btnAccept: 'Ok em <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //https://www.facebook.com/messages/t/100007730149351. VD: https://m.me/nam.nodemy
 }
